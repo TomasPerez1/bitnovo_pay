0b0c3c999f91d7be91161b4a91657571f4222202
tomasPerez <tomasperez.henry@gmail.com>
2025-02-18 15:11:22 -0300

    other plugin import

--- bitnovo_pay/eslint.config.mjs
+++ bitnovo_pay/eslint.config.mjs
@@ -1,12 +1,16 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  plugins: {
+    prettier: eslintPluginPrettier,
+  },
 });
 
 const eslintConfig = [
