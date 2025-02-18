e6545b727275f13ade2548cfb774e56df3ff322b
tomasPerez <tomasperez.henry@gmail.com>
2025-02-18 15:15:42 -0300

    del prettieer

--- bitnovo_pay/eslint.config.mjs
+++ bitnovo_pay/eslint.config.mjs
@@ -1,16 +1,16 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+// import eslintPluginPrettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  plugins: {
-    prettier: eslintPluginPrettier,
-  },
+  // plugins: {
+  //   prettier: eslintPluginPrettier,
+  // },
 });
 
 const eslintConfig = [
@@ -18,7 +18,7 @@ const eslintConfig = [
   {
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     rules: {
-      "prettier/prettier": "error",
+      'allowImportingTsExtensions': "off"
     }
   }
 ];
